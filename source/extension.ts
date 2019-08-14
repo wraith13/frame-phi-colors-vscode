472220f5360ca4870afbf83267a2b116bfc26f79
道化師 <wraith13@hotmail.co.jp>
2019-08-15 00:49:34 +0900

    🛠 実装 ( WIP )

--- source/extension.ts
+++ source/extension.ts
@@ -2,6 +2,62 @@ import * as vscode from 'vscode';
 
 export module FramePhiColors
 {
+    const applicationKey = "framePhiColors";
+
+    class Config<valueT>
+    {
+        public constructor
+        (
+            public name: string,
+            public defaultValue: valueT,
+            public validator?: (value: valueT) => boolean,
+            public minValue?: valueT,
+            public maxValue?: valueT
+        )
+        {
+
+        }
+
+        regulate = (rawKey: string, value: valueT): valueT =>
+        {
+            let result = value;
+            if (this.validator && !this.validator(result))
+            {
+                // settings.json をテキストとして直接編集してる時はともかく GUI での編集時に無駄にエラー表示が行われてしまうので、エンドユーザーに対するエラー表示は行わない。
+                //vscode.window.showErrorMessage(`${rawKey} setting value is invalid! Please check your settings.`);
+                console.error(`${rawKey} setting value is invalid! Please check your settings.`);
+                result = this.defaultValue;
+            }
+            else
+            {
+                if (undefined !== this.minValue && result < this.minValue)
+                {
+                    result = this.minValue;
+                }
+                else
+                if (undefined !== this.maxValue && this.maxValue < result)
+                {
+                    result = this.maxValue;
+                }
+            }
+            return result;
+        }
+
+        public get = (): valueT =>
+        {
+            let result = <valueT>vscode.workspace.getConfiguration(applicationKey)[this.name];
+            if (undefined === result)
+            {
+                result = this.defaultValue;
+            }
+            else
+            {
+                result = this.regulate(`${applicationKey}.${this.name}`, result);
+            }
+            return result;
+        }
+    }
+
     export const initialize = (context: vscode.ExtensionContext): void =>
     {
         console.log('Congratulations, your extension "frame-phi-colors" is now active!');
